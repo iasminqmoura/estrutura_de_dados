64ea2a1c1f8b6b5b040c63d83c6380d0396afa70
iasminqmoura <36743683+iasminqmoura@users.noreply.github.com>
2019-04-15 22:21:19 -0300

    Update Exercicio03.c

--- Lista 06/Exercicio03.c
+++ Lista 06/Exercicio03.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 
-//03) Dado dois vetores aleatórios, A (5 elementos) e B (8 elementos), faça um programa em C que imprima todos os elementos comuns aos dois vetores.
+//03) Dado dois vetores aleatÃ³rios, A (5 elementos) e B (8 elementos), faÃ§a um programa em C que imprima todos os elementos comuns aos dois vetores.
 
 int main() 
 {	
